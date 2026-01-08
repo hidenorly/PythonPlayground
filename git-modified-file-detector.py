313aa7a128af14a60025172e4b0643d40ae15469
hidenorly <twitte.harold@gmail.com>
2026-01-09 04:08:03 +0900

    Add --git and --temp support on git-modified-file-detector.py

--- git-modified-file-detector.py
+++ git-modified-file-detector.py
@@ -15,32 +15,48 @@
 #   limitations under the License.
 
 
+import os
 import argparse
 from GitUtil import GitUtil
 
+class FileUtil:
+	@staticmethod
+	def write_file(path, lines):
+		with open(path, 'w', encoding='utf-8', errors='ignore') as f:
+			f.writelines(lines)
+
 if __name__=="__main__":
 	parser = argparse.ArgumentParser(description='modified file detectpr', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('-t', '--target', action='store', default=".", help='specify git directory')
+	parser.add_argument('-g', '--git', action='store', default=".", help='specify git directory')
+	parser.add_argument('-t', '--temp', action='store', default="~/tmp", help='specify temp directory')
 	parser.add_argument('-b', '--branch', action='store', default="", help='specify branch. use .. for compare')
 	parser.add_argument('-i', '--interested', action='store', default="h|hxx|hpp|proto|capnp|dart", help='specify interested file extensions (separator:|)')
 
 	args = parser.parse_args()
 
+	git_path = os.path.abspath(os.path.expanduser(args.git))
+	tmp_path = os.path.abspath(os.path.expanduser(args.temp))
 	branches = args.branch.split("..")
 	interests = args.interested.split("|")
+
 	file_extensions = []
 	for ext in interests:
 		file_extensions.append(f".{ext}")
 	if len(branches)!=2:
 		if not branches[0]:
 			branches[0]="HEAD"
-		branches = [GitUtil.get_tail(args.target), branches[0]]
-	changed_files = GitUtil.changed_files(args.target, branches[0], branches[1], file_extensions)
+		branches = [GitUtil.get_tail(git_path), branches[0]]
+	changed_files = GitUtil.changed_files(git_path, branches[0], branches[1], file_extensions)
 	for file in changed_files:
 		print(file)
-		old = GitUtil.show(args.target, branches[0], file)
-		new = GitUtil.show(args.target, branches[1], file)
-		print("old:")
-		print(str(old))
-		print("\nnew:")
-		print(str(new))
+		contents = []
+		contents.append( GitUtil.show(git_path, branches[0], file) )
+		contents.append( GitUtil.show(git_path, branches[1], file) )
+		if contents[0] and contents[1]:
+			git_name = GitUtil.get_git_name(git_path)
+			temp_out_path = os.path.join(tmp_path, git_name)
+			for branch, content in zip(branches, contents):
+				temp_out_file_dir = os.path.join(temp_out_path, branch)
+				if not os.path.exists(temp_out_file_dir):
+					os.makedirs(temp_out_file_dir)
+				FileUtil.write_file( os.path.join(temp_out_file_dir, file), content )
