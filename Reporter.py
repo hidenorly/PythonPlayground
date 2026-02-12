60fc5d32f1e668b6d27489328e9c33d4064ae2e2
hidenorly <twitte.harold@gmail.com>
2026-02-12 13:58:00 +0900

    Add support on dict data

--- Reporter.py
+++ Reporter.py
@@ -17,42 +17,43 @@
 import os
 
 
-# data = {
-#	section_1:[
-#		{key1:data1_1, key2:data2_1, key3:data3_1},
-#		{key1:data1_2, key2:data2_2, key3:data3_2},
-#		...
-#	],
-#	section_2:[
-#	]
-# }
-
-
 class Reporter:
 	def __init__(self):
 		pass
 
 	def report(self, data, print_section_keys=[]):
-		for section, section_data in data.items():
-			self.print_section(section)
+		# check section, data structure
+		if isinstance(data, dict):
+			for section, section_data in data.items():
+				self.print_section(section)
 
-			# extract col keys
-			section_keys_ = set()
-			section_keys = []
-			for line_data in section_data:
-				for key, col in line_data.items():
-					if not key in section_keys_:
-						section_keys_.add(key)
-						section_keys.append(key)
-			# dump cols
-			if not print_section_keys:
-				print_section_keys = section_keys
-			self.print_section_keys(print_section_keys)
+				# check data is array
+				if isinstance(section_data, list):
+					# check data is dict
+					if isinstance(section_data[0], dict):
+						# extract col keys
+						section_keys_ = set()
+						section_keys = []
+						for line_data in section_data:
+							for key, col in line_data.items():
+								if not key in section_keys_:
+									section_keys_.add(key)
+									section_keys.append(key)
+						# dump cols
+						if not print_section_keys:
+							print_section_keys = section_keys
+						self.print_section_keys(print_section_keys)
 
-			# dump data
-			for line_data in section_data:
-				self.print_line_data(line_data, print_section_keys)
+						# dump data
+						for line_data in section_data:
+							self.print_line_dict_data(line_data, print_section_keys)
 
+					# check data is array
+					elif isinstance(section_data[0], list):
+						if print_section_keys:
+							self.print_section_keys(print_section_keys)
+						for line_data in section_data:
+							self.print_line_list_data(line_data)
 
 	def print_section(self, section):
 		print("")
@@ -62,13 +63,24 @@ class Reporter:
 	def print_section_keys(self, section_keys):
 		print(",".join(section_keys))
 
-	def print_line_data(self, line_data, print_section_keys):
+	def print_line_dict_data(self, line_data, print_section_keys):
 		print_str = ""
 		for key in print_section_keys:
 			if print_str:
 				print_str += ","
 			if key in line_data:
-				print_str += f"{line_data[key]}"
+				print_str += line_data[key]
+			else:
+				print_str += " "
+		print(print_str)
+
+	def print_line_list_data(self, line_data):
+		print_str = ""
+		for col in line_data:
+			if print_str:
+				print_str += ","
+			if col:
+				print_str += col
 			else:
 				print_str += " "
 		print(print_str)
@@ -90,3 +102,16 @@ if __name__=="__main__":
 
 	report = Reporter()
 	report.report(data)
+
+	data2 = {
+		"section_1":[
+			["data1_1", "data2_1", "data3_1"],
+			["data1_2", "data2_2", "data3_2"],
+		],
+		"section_2":[
+			["data1_1", "data2_1", "data3_1"],
+			["data1_2", "data2_2", "data3_2"],
+		]
+	}
+
+	report.report(data2, ["key1","key2","key3"])
